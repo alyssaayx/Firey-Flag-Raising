93e4721245c5cb0b229f3d8853aebdf77aaf7640
Alyssa Aw Yong <alyssa.awyong@gmail.com>
2021-09-06 10:28:05 +0800

    Made a basic flag raising app (adding working buttons)

--- Firey Flag Raising/FlagRaisingView.swift
+++ Firey Flag Raising/FlagRaisingView.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 
 struct FlagRaisingView: View {
+    
+    @State var offset: CGFloat = 20
+    
     var body: some View {
         VStack {
             HStack(alignment: .bottom, spacing: -10) {
@@ -17,19 +20,20 @@ struct FlagRaisingView: View {
                 
                 Text("🇸🇬")
                     .font(.system(size: 100))
+                    .offset(x: 0, y: offset)
             }
-        }
         
         Button("Raise Flag") {
-            
+            offset -= 10
         }
         .padding()
         
         Button("Reset") {
-            
+           offset = 20
         }
         .padding()
     }
+}
 }
 
 struct FlagRaisingView_Previews: PreviewProvider {

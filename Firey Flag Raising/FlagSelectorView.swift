b6de1deb9ef5f9a14cca1fc7b83702372df41812
Alyssa Aw Yong <alyssa.awyong@gmail.com>
2021-09-06 10:44:41 +0800

    Decoding FlagData.json

--- Firey Flag Raising/FlagSelectorView.swift
+++ Firey Flag Raising/FlagSelectorView.swift
@@ -8,6 +8,17 @@
 import SwiftUI
 
 struct FlagSelectorView: View {
+    
+    var flags: [Flag] {
+        let flagDataURL = Bundle.main.url(forResource: "FlagData", withExtension: "json")!
+        
+        let decoder = JSONDecoder()
+        let data = try! Data(contentsOf: flagDataURL)
+        
+        return try! decoder.decode([Flag].self, from: data)
+    }
+    
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }

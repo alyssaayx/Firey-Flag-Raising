1a536a01f215820c7872c027cfd2d63d60d6ca1f
Alyssa Aw Yong <alyssa.awyong@gmail.com>
2021-09-06 10:10:38 +0800

    Initial Commit

---
+++ Firey Flag Raising/Firey_Flag_RaisingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Firey_Flag_RaisingApp.swift
+//  Firey Flag Raising
+//
+//  Created by rgs on 6/9/21.
+//
+
+import SwiftUI
+
+@main
+struct Firey_Flag_RaisingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
